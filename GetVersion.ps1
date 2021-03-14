cdaece2b4107148b81c1ff64c574b6857ffc8a51
Toby Smith <tobysmith568@hotmail.co.uk>
2021-03-14 17:37:06 +0000

    Set GetVersion.ps1 to download the latest SPDX release

--- GetVersion.ps1
+++ GetVersion.ps1
@@ -1,18 +1,40 @@
+function Download-Release {
+	param (
+		[string]$url,
+		[string]$destination
+	);
+
+	# create temp with zip extension (or Expand will complain)
+	$tmp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } -PassThru;
+	#download
+	Invoke-WebRequest -OutFile $tmp -Uri $url;
+	#exract to same folder 
+	$tmp | Expand-Archive -DestinationPath $destination -Force;
+	# remove temporary file
+	$tmp | Remove-Item;
+}
+
+# Get SPDX Latest Release Version
 $spdxReleasesUrl = "https://api.github.com/repos/spdx/license-list-data/releases";
 $spdxReleases = Invoke-RestMethod -Uri $spdxReleasesUrl -ContentType "application/json";
 $latestSpdxRelease = $spdxReleases | Select-Object -First 1;
 $latestSpdxReleaseVersion = $latestSpdxRelease.tag_name;
 
-Move-Item -Path "../_spdx_license-list-data/$latestSpdxReleaseVersion" -Destination "../license-list-data"
+# Get SPDX Latest Release
+
+$latestSpdxReleaseDataUrl = "https://github.com/spdx/license-list-data/archive/$latestSpdxReleaseVersion.zip";
+Download-Release $latestSpdxReleaseDataUrl "../license-list-data-parent";
 
 if ($latestSpdxReleaseVersion[0] -eq 'v')
 {
 	$latestSpdxReleaseVersion = $latestSpdxReleaseVersion.Substring(1);
 }
 
+Move-Item -Path "../license-list-data-parent/license-list-data-$latestSpdxReleaseVersion" -Destination "../license-list-data";
+
 $nugetReleasesUrl = "https://api.nuget.org/v3-flatcontainer/LicenseIdentifiers/index.json";
 $nugetReleases = Invoke-RestMethod -Uri $nugetReleasesUrl -ContentType "application/json";
-$latestNugetReleaseVersion = $nugetReleases.versions | Select-Object -Last 1
+$latestNugetReleaseVersion = $nugetReleases.versions | Select-Object -Last 1;
 
 Write-Host "Latest SPDX Release: $latestSpdxReleaseVersion";
 Write-Host "Latest NuGet Release: $latestNugetReleaseVersion";
@@ -22,5 +44,5 @@ if ($latestSpdxReleaseVersion -eq $latestNugetReleaseVersion)
 	Write-Error "Version $latestSpdxReleaseVersion already published";
 }else
 {
-	Write-Host "##vso[task.setvariable variable=latestVersion]$latestSpdxReleaseVersion"
+	Write-Host "##vso[task.setvariable variable=latestVersion]$latestSpdxReleaseVersion";
 }
